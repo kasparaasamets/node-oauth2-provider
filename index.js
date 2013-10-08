5661515dbc9d695e180532a307fd3f6ed1edc26f
Kaspar Aasamets <kaspar.aasamets@cbts.eu>
2013-10-08 13:09:31 +0300

    2-legged OAuth2 support

--- index.js
+++ index.js
@@ -115,166 +115,40 @@ OAuth2Provider.prototype.login = function() {
 
 OAuth2Provider.prototype.oauth = function() {
   var self = this;
-
   return function(req, res, next) {
     var uri = ~req.url.indexOf('?') ? req.url.substr(0, req.url.indexOf('?')) : req.url;
-
-    if(req.method == 'GET' && self.options.authorize_uri == uri) {
-      var    client_id = req.query.client_id,
-          redirect_uri = req.query.redirect_uri;
-
-      if(!client_id || !redirect_uri) {
-        res.writeHead(400);
-        return res.end('client_id and redirect_uri required');
-      }
-
-      // authorization form will be POSTed to same URL, so we'll have all params
-      var authorize_url = req.url;
-
-      self.emit('enforce_login', req, res, authorize_url, function(user_id) {
-        // store user_id in an HMAC-protected encrypted query param
-        authorize_url += '&' + querystring.stringify({x_user_id: self.serializer.stringify(user_id)});
-
-        // user is logged in, render approval page
-        self.emit('authorize_form', req, res, client_id, authorize_url);
+    if(req.method == 'POST' && '/oauth/authorize' == uri) {
+      var client_id = req.body.client_id,
+          code = serializer.randomString(128);
+      self.emit('save_grant', req, client_id, code, function() {
+        var extras = {
+          code: code,
+        };
+        res.send({'request_token': code});
       });
-
-    } else if(req.method == 'POST' && self.options.authorize_uri == uri) {
-      var     client_id = (req.query.client_id || req.body.client_id),
-           redirect_uri = (req.query.redirect_uri || req.body.redirect_uri),
-          response_type = (req.query.response_type || req.body.response_type) || 'code',
-                  state = (req.query.state || req.body.state),
-              x_user_id = (req.query.x_user_id || req.body.x_user_id);
-
-      var url = redirect_uri;
-
-      switch(response_type) {
-        case 'code': url += '?'; break;
-        case 'token': url += '#'; break;
-        default:
-          res.writeHead(400);
-          return res.end('invalid response_type requested');
-      }
-
-      if('allow' in req.body) {
-        if('token' == response_type) {
-          var user_id;
-
-          try {
-            user_id = self.serializer.parse(x_user_id);
-          } catch(e) {
-            console.error('allow/token error', e.stack);
-
-            res.writeHead(500);
-            return res.end(e.message);
-          }
-
-          self.emit('create_access_token', user_id, client_id, function(extra_data,token_options) {
-            var atok = self.generateAccessToken(user_id, client_id, extra_data, token_options);
-
-            if(self.listeners('save_access_token').length > 0)
-              self.emit('save_access_token', user_id, client_id, atok);
-
-            url += querystring.stringify(atok);
-
-            res.writeHead(303, {Location: url});
-            res.end();
-          });
-        } else {
-          var code = serializer.randomString(128);
-
-          self.emit('save_grant', req, client_id, code, function() {
-            var extras = {
-              code: code,
-            };
-
-            // pass back anti-CSRF opaque value
-            if(state)
-              extras['state'] = state;
-
-            url += querystring.stringify(extras);
-
-            res.writeHead(303, {Location: url});
-            res.end();
-          });
-        }
-      } else {
-        url += querystring.stringify({error: 'access_denied'});
-
-        res.writeHead(303, {Location: url});
-        res.end();
-      }
-
-    } else if(req.method == 'POST' && self.options.access_token_uri == uri) {
-      var     client_id = req.body.client_id,
+    } else if(req.method == 'POST' && '/oauth/access_token' == uri) {
+      var client_id = req.body.client_id,
           client_secret = req.body.client_secret,
-           redirect_uri = req.body.redirect_uri,
-                   code = req.body.code;
-
-      if(!client_id || !client_secret) {
-        var authorization = parse_authorization(req.headers.authorization);
-
-        if(!authorization) {
+          code = req.body.code;
+      self.emit('lookup_grant', client_id, client_secret, code, function(err, user_id) {
+        if (err) {
           res.writeHead(400);
-          return res.end('client_id and client_secret required');
+          return res.end(err.message);
         }
-
-        client_id = authorization[0];
-        client_secret = authorization[1];
-      }
-
-      if('password' == req.body.grant_type) {
-        if(self.listeners('client_auth').length == 0) {
-          res.writeHead(401);
-          return res.end('client authentication not supported');
-        }
-
-        self.emit('client_auth', client_id, client_secret, req.body.username, req.body.password, function(err, user_id) {
-          if(err) {
-            res.writeHead(401);
-            return res.end(err.message);
+        res.writeHead(200, {'Content-type': 'application/json'});
+        self.emit('create_access_token', user_id, client_id, function(extra_data) {
+          var atok = self.generateAccessToken(user_id, client_id, extra_data);
+          if (self.listeners('save_access_token').length > 0) {
+            self.emit('save_access_token', user_id, client_id, atok);
           }
-
-          res.writeHead(200, {'Content-type': 'application/json'});
-
-          self._createAccessToken(user_id, client_id, function(atok) {
-            res.end(JSON.stringify(atok));
-          });
+          res.end(JSON.stringify(self.generateAccessToken(user_id, client_id, extra_data)));
         });
-      } else {
-        self.emit('lookup_grant', client_id, client_secret, code, function(err, user_id) {
-          if(err) {
-            res.writeHead(400);
-            return res.end(err.message);
-          }
-
-          res.writeHead(200, {'Content-type': 'application/json'});
-
-          self._createAccessToken(user_id, client_id, function(atok) {
-            self.emit('remove_grant', user_id, client_id, code);
-
-            res.end(JSON.stringify(atok));
-          });
-        });
-      }
-
+        self.emit('remove_grant', user_id, client_id, code);
+      });
     } else {
       return next();
     }
   };
-};
-
-OAuth2Provider.prototype._createAccessToken = function(user_id, client_id, cb) {
-  var self = this;
-
-  this.emit('create_access_token', user_id, client_id, function(extra_data, token_options) {
-    var atok = self.generateAccessToken(user_id, client_id, extra_data, token_options);
-
-    if(self.listeners('save_access_token').length > 0)
-      self.emit('save_access_token', user_id, client_id, atok);
-
-    return cb(atok);
-  });
 };
 
 exports.OAuth2Provider = OAuth2Provider;
